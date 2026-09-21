3d2aaf397d2bba2c1f5d9f3eec30ce8a8da11746
Ihsaan7 <ihsaan2215@gmail.com>
2026-09-21 12:04:29 +0500

    Server basic config done

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express, { Request , Response } from "express"
+import cors from "cors"
+import  { requestLogger } from "./middlewares/logger.middleware.js"
+import productRoute from "./routes/product.route.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+
+app.get("/health" , (_req: Request , res:Response)=>
+    {
+        res.status(200).json(
+            {
+                success:true,
+                message:"Typescript Epxress server is Healthy"
+            })
+    })
+
+// Routes
+app.use("/api/v1/products", productRoute)
+
+// 404 Catch-all
+app.use((req: Request , res:Response)=>
+    {
+        res.status(404).json({
+            success: false,
+            message:`Route ${req.originalUrl} not found on this server`
+        })
+    })
+
+export default app
